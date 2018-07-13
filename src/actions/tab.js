284a7679529561346b3ed2abc60c44038da4de39
Harry Reeder <harry@harryreeder.co.uk>
2018-07-13 23:55:13 +0100

    Chang MAINS to jsx this time

--- src/actions/tab.js
+++ src/actions/tab.js
@@ -3,6 +3,8 @@ import { filterByDiet } from '../util';
 export function setMenuItem(type, menu, diet) {
     let entries = filterByDiet(menu, diet)
 
+    // Read the ~~ as Math.floor
+    // Essentially we're getting a random entry from entries
     let chosen = entries[~~(Math.random()*entries.length)]
 
     return { type: type, payload: chosen }
